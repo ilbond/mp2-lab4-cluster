e55781d2fccec3501d715c422de5784f5811f406
Ilya Bondarenko <ilbond2000@yandex.ru>
2019-11-18 19:37:47 +0300

    ура

--- cluster/cluster/Queue.h
+++ cluster/cluster/Queue.h
@@ -1,1 +1,45 @@
 #pragma once
+#include "TList.h"
+
+template <typename T>
+class queue
+{
+	Node <T> *pFirst;
+public:
+	queue() :pFirst(nullptr) {}
+	~queue() {
+		while (pFirst != nullptr) {
+			Node *p = pFirst;
+			pFirst = p->pNext;
+			delete p;
+		}
+	}
+	bool isEmpty() {
+		return pFirst == nullptr;
+	}
+	void push(T val) {
+		Node *p = new Node;
+		p->data = val;
+		Node *pp = pFirst;
+		while (pp->pNext != nullptr) {
+			pp = pp->pNext;
+		}
+		pp->pNext = p;
+	}
+	T GetFirst() {
+		return pFirst;
+	}
+	T pop() {
+		if (isEmpty) {
+			throw 1;
+		}
+		T val = pFirst->data;
+		Node *p = pFirst->pNext;
+		delete pFirst;
+		pFirst = p;
+		return val;
+
+	}
+};
+
+
--- cluster/cluster/TList.h
+++ cluster/cluster/TList.h
@@ -1,8 +1,38 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
+//Звено списка
+template <typename T>
+struct Node
+{
+	Node * pNext;
+	T data;
+	Node(T data = T(), Node * pNext = nullptr)
+	{
+		this->data = data;
+		this->pNext = pNext;
+	}
+	~Node() { }
+};
+
+//Односвязый список
+template <typename T>
 class TList
 {
+private:
+	int size;
+	Node <T> * pFirst;
 public:
 	TList();
-	~TList();
+	~TList() { delete[] head; }
+	void InsertLast(T val); // Вставка в конец
+	void InsertFirst(T val); // Вставка в начало
+	void Insert(T val, int pos); // Вставка на позицию
+	void DeleteElem(int pos); // Удаление n-ого элемента
+	int len() { return size; } // Узнать длину
+	Node <T> * FindElem(T); // Поиск элемента
+
 };
 
